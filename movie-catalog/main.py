d587b1abcf5a63b03bbfa872df25e527bdbbc691
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-05-12 00:42:16 +0300

    Add root view

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,3 +1,28 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
+import uvicorn
 
-app = FastAPI(title="Movie Catalog")
+app = FastAPI(
+    title="Movie Catalog",
+)
+
+
+@app.get("/")
+def get_docs(
+    request: Request,
+):
+    docs_url = request.url.replace(
+        path="/docs",
+    )
+    return {
+        "docs": str(docs_url),
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        reload=True,
+    )
