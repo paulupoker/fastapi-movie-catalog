0880fd4f9a7f079e1f3ec25d1738d639d0711189
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-07-06 02:05:45 +0300

    Extend schema: add class for create movie

--- movie-catalog/schemas/movie_description.py
+++ movie-catalog/schemas/movie_description.py
@@ -1,7 +1,9 @@
 from pydantic import (
     BaseModel,
     AnyHttpUrl,
+    Field,
 )
+from typing import Annotated
 
 
 class MovieDescriptionBase(BaseModel):
@@ -20,4 +22,16 @@ class MovieDescription(MovieDescriptionBase):
     Movie description model.
     """
 
-    pass
+
+class MovieDescriptionCreate(BaseModel):
+    """
+    Model for creation movie description.
+    """
+
+    title: str = Field(max_length=100)
+    description: str = Field(max_length=1000)
+    genre: str = Field(max_length=100)
+    year: int = Field(ge=1900, le=3000)
+    director: str = Field(max_length=100)
+    rating: float = Field(ge=1, le=10)
+    url: Annotated[str, AnyHttpUrl]
