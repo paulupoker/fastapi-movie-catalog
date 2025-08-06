1b7e9601ef6e32a3614cb0a34e6bc063d2675bd9
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-08-06 22:58:39 +0300

    Fix methods for working with redis

--- movie-catalog/api/api_v1/movies/crud.py
+++ movie-catalog/api/api_v1/movies/crud.py
@@ -54,26 +54,34 @@ class MoviesStorage(BaseModel):
         logger.warning("Recovered data from storage file")
 
     def get(self) -> list[Movie]:
-        return list(self.slug_to_movie.values())
+        return [
+            Movie.model_validate_json(value)
+            for value in redis.hvals(name=config.REDIS_MOVIES_HASH_NAME)
+        ]
 
     def get_by_slug(self, slug: str) -> Movie | None:
-        return self.slug_to_movie.get(slug)
+        if movie := redis.hget(name=config.REDIS_MOVIES_HASH_NAME, key=slug):
+            return Movie.model_validate_json(movie)
+        return None
 
-    def create(self, movie_in: MovieCreate) -> Movie:
-        movie = Movie(
-            **movie_in.model_dump(),
-        )
+    def save_movie(self, movie: Movie) -> None:
         redis.hset(
             name=config.REDIS_MOVIES_HASH_NAME,
             key=movie.slug,
             value=movie.model_dump_json(),
         )
+
+    def create(self, movie_in: MovieCreate) -> Movie:
+        movie = Movie(
+            **movie_in.model_dump(),
+        )
+        self.save_movie(movie)
         logger.info("Created a new movie with slug: %r", movie.slug)
         return movie
 
     def delete_by_slug(self, slug: str) -> None:
-        self.slug_to_movie.pop(slug, None)
-        logger.info("Deleted the movie with slug: %r", slug)
+        if redis.hdel(config.REDIS_MOVIES_HASH_NAME, slug):
+            logger.info("Deleted the movie with slug: %r", slug)
 
     def delete(self, movie: Movie) -> None:
         self.delete_by_slug(slug=movie.slug)
@@ -85,6 +93,7 @@ class MoviesStorage(BaseModel):
     ) -> Movie:
         for field_name, value in movie_in:
             setattr(movie, field_name, value)
+        self.save_movie(movie)
         logger.info("Updated the movie with slug: %r", movie.slug)
         return movie
 
@@ -95,6 +104,7 @@ class MoviesStorage(BaseModel):
     ) -> Movie:
         for field_name, value in movie_in.model_dump(exclude_unset=True).items():
             setattr(movie, field_name, value)
+        self.save_movie(movie)
         logger.info("Partially updated the movie with slug: %r", movie.slug)
         return movie
 
