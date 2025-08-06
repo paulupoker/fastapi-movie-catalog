6d0f9053db7a39f018b9b33be2f44c42cc2a98a4
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-08-06 23:12:23 +0300

    Remove unnecessary actions to save to disk

--- movie-catalog/api/api_v1/movies/views/list_views.py
+++ movie-catalog/api/api_v1/movies/views/list_views.py
@@ -6,7 +6,6 @@ from fastapi import (
 
 from api.api_v1.movies.crud import storage
 from api.api_v1.movies.dependencies import (
-    save_storage_state,
     api_token_or_user_basic_auth_required_for_unsafe_methods,
 )
 from schemas.movies import (
@@ -19,7 +18,6 @@ router = APIRouter(
     prefix="/movies",
     tags=["Movies"],
     dependencies=[
-        Depends(save_storage_state),
         Depends(api_token_or_user_basic_auth_required_for_unsafe_methods),
     ],
     responses={
