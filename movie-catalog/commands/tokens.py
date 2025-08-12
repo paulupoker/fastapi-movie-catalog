278689c9e18271b9f1fe80a8ff20eda71414592f
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-08-12 14:29:05 +0300

    Add checking tokens to CLI management commands

---
+++ movie-catalog/commands/tokens.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+import typer
+from rich import print
+
+from api.api_v1.auth.services import redis_tokens
+
+app = typer.Typer(
+    name="token",
+    help="Tokens management",
+    no_args_is_help=True,
+    rich_markup_mode="rich",
+)
+
+
+@app.command()
+def check(
+    token: Annotated[
+        str,
+        typer.Argument(help="The token to check"),
+    ],
+):
+    """
+    Check if the passed token is valid - exists or not.
+    """
+    print(
+        f"Token [bold]{token}[/bold]",
+        (
+            "[green]exists[/green]."
+            if redis_tokens.token_exists(token)
+            else "[red]does not exist[/red]."
+        ),
+    )
