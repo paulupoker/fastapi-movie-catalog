23d524247cfa88fdc671b89f9d3f16dda2895298
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-12-04 19:55:49 +0300

    Create parametrize test for root_view

--- movie-catalog/api/main_views.py
+++ movie-catalog/api/main_views.py
@@ -7,8 +7,12 @@ router = APIRouter()
 
 
 @router.get("/")
-def get_docs(request: Request) -> dict[str, str]:
+def get_docs(
+    request: Request,
+    name: str = "World",
+) -> dict[str, str]:
     docs_url = request.url.replace(path="/docs")
     return {
+        "message": f"Hello, {name}!",
         "docs": str(docs_url),
     }
---
+++ movie-catalog/testing/test_api/test_views.py
@@ -0,0 +1,34 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_root_view() -> None:
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    response_data = response.json()
+    expected_message = "Hello, World!"
+    assert response_data["message"] == expected_message, response_data
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        # TODO: fake data
+        "John",
+        "John Doe",
+        "!@#$%",
+        "",
+    ],
+)
+def test_root_view_custom_name(name: str) -> None:
+    query = {"name": name}
+    response = client.get("/", params=query)
+    assert response.status_code == status.HTTP_200_OK, response.text
+    response_data = response.json()
+    expected_message = f"Hello, {name}!"
+    assert response_data["message"] == expected_message, response_data
