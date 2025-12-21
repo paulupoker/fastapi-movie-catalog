8539e3d8d2412e27a42dff3f0b912fcdc6f713c5
Paul Dvoinishev <dvoynishevp@gmail.com>
2025-12-21 23:52:35 +0300

    Add test for deleting movie

---
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_details_views/test_delete.py
@@ -0,0 +1,41 @@
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi.testclient import TestClient
+from pydantic import AnyHttpUrl
+from starlette import status
+
+from api.api_v1.movies.crud import storage
+from main import app
+from schemas.movies import Movie, MovieCreate
+
+
+def create_movie(slug: str) -> Movie:
+    movie_in = MovieCreate(
+        title="Slug",
+        description="SlugSlugSlug",
+        genre="Slug",
+        year=2020,
+        director="Slug Slug",
+        rating=8.0,
+        url=AnyHttpUrl("https://www.example.com"),
+        slug=slug,
+    )
+    return storage.create(movie_in)
+
+
+@pytest.fixture(
+    params=[
+        pytest.param("some-slug", id="common slug"),
+        pytest.param("abc", id="min length slug"),
+        pytest.param("abc" * 10, id="max length slug"),
+    ],
+)
+def movie(request: SubRequest) -> Movie:
+    return create_movie(request.param)
+
+
+def test_delete(movie: Movie, auth_client: TestClient) -> None:
+    url = app.url_path_for("delete_movie", slug=movie.slug)
+    response = auth_client.delete(url=url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT, response.text
+    assert not storage.exists(movie.slug)
