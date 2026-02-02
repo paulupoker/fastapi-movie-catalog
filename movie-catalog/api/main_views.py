1af72b14aae3227b6d815690f9f61ad670470fe4
Paul Dvoinishev <dvoynishevp@gmail.com>
2026-02-02 21:33:29 +0300

    Start preparing workflow for python checks

--- movie-catalog/api/main_views.py
+++ movie-catalog/api/main_views.py
@@ -7,10 +7,7 @@ router = APIRouter()
 
 
 @router.get("/")
-def get_docs(
-    request: Request,
-    name: str = "World",
-) -> dict[str, str]:
+def get_docs(request: Request, name: str = "World") -> dict[str, str]:
     docs_url = request.url.replace(path="/docs")
     return {
         "message": f"Hello, {name}!",
