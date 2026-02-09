b30b187187b072013db8b4089f6121062260d844
Paul Dvoinishev <dvoynishevp@gmail.com>
2026-02-09 22:53:48 +0300

    validate redis database numbers

--- movie-catalog/core/config.py
+++ movie-catalog/core/config.py
@@ -1,8 +1,8 @@
 import logging
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Self
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +38,14 @@ class RedisDBConfig(BaseModel):
     tokens: int = 1
     users: int = 2
     movies: int = 3
+
+    @model_validator(mode="after")
+    def validate_dbs_numbers_unique(self) -> Self:
+        db_values = list(self.model_dump().values())
+        msg = "Database numbers should be unique!"
+        if len(set(db_values)) != len(db_values):
+            raise ValueError(msg)
+        return self
 
 
 class RedisCollectionsNamesConfig(BaseModel):
